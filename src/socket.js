59d03034eb9b3791e01e64e8a94e8fff6bb7bace
DhirajRathore <dhirajs8296@gmail.com>
2023-11-06 17:55:41 +0530

    minor fix

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 const URL =
   process.env.NODE_ENV === "production"
-    ? "https://dsr-sketch-book.onrender.com/"
+    ? "https://dsr-sketch-book.onrender.com"
     : "http://localhost:5000";
 
 export const socket = io(URL);
